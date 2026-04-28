ea1e6ee191dd8015ba01b5e31d15a81b6ca4cd1b
Lorenzo Celani <lorenzo.celani@student.unicam.it>
2026-04-29 01:49:21 +0200

    Modificata ArmaturaIronMan

--- app/src/main/java/it/unicam/cs/mpgc/rpg130836/ArmaturaIronMan.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130836/ArmaturaIronMan.java
@@ -1,2 +1,7 @@
 package it.unicam.cs.mpgc.rpg130836;
 
+public class ArmaturaIronMan extends Armatura {
+    public ArmaturaIronMan() {
+        super("Armatura Iron Man", new Repulsore());
+    }
+}
