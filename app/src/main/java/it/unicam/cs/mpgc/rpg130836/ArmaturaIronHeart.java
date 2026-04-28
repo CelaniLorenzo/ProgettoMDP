5fe372eac21370a9d680e75bc1842651c7ecf193
Lorenzo Celani <lorenzo.celani@student.unicam.it>
2026-04-29 01:47:03 +0200

    Modificata ArmaturaIronHeart

--- app/src/main/java/it/unicam/cs/mpgc/rpg130836/ArmaturaIronHeart.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130836/ArmaturaIronHeart.java
@@ -1,2 +1,7 @@
 package it.unicam.cs.mpgc.rpg130836;
 
+public class ArmaturaIronHeart extends Armatura {
+    public ArmaturaIronHeart() {
+        super("Armatura Iron Heart", new BollaRosa());
+    }
+}
