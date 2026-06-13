25ff46d5574ac220a4b861f8e044eac461bc1aaa
Lorenzo Celani <lorenzo.celani@student.unicam.it>
2026-06-13 16:07:06 +0200

    Aggiunta di commenti

--- app/src/main/java/it/unicam/cs/mpgc/rpg130836/grafica/Avvio/AccessoSchermataGioco.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130836/grafica/Avvio/AccessoSchermataGioco.java
@@ -2,5 +2,8 @@ package it.unicam.cs.mpgc.rpg130836.grafica.Avvio;
 
 public interface AccessoSchermataGioco {
 
+    /*
+     * Mostra la schermata principale del gioco.
+     */
     void mostraSchermataGioco();
 }
